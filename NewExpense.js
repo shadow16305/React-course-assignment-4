29afa8fde2ddf818a894e4f257db7a7012ecbb61
shadow16305 <121637057+shadow16305@users.noreply.github.com>
2023-08-04 21:01:44 +0200

    Create NewExpense.js

---
+++ NewExpense.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+
+const NewExpense = (props) => {
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        props.onAddExpense(expenseData);
+        setFormIsOpen(false);
+    };
+
+    const [formOpen, setFormIsOpen] = useState(false);
+
+    const addExpenseHandler = () => {
+        setFormIsOpen(true);
+    };
+
+    const cancelHandler = () => {
+        setFormIsOpen(false);
+    };
+
+
+    return (
+        <div className='new-expense'>
+            {formOpen && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} cancel={cancelHandler} />}
+            {!formOpen && <button onClick={addExpenseHandler}>Add Expense</button>}
+        </div>
+    );
+};
+
+export default NewExpense;
